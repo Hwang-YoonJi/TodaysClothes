8127c18cc14f00386d7602eb37ba938f4e2543fe
Hwang-yoonji <kaci00@naver.com>
2021-09-27 15:04:55 +0900

    날씨에 맞는 옷 추천

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, ScrollView } from "react-native";
+import { Text, View, ScrollView, Image } from "react-native";
 
 const HomeScreen = () => {
   return (
@@ -16,25 +16,57 @@ const HomeScreen = () => {
           <Text style={{ fontSize: 20, color: "#fff", marginTop: 5 }}>
             맑음
           </Text>
-          <Text style={{ fontSize: 70, color: "#fff" }}>26°</Text>
-          <Text style={{ fontSize: 20, color: "#fff" }}>체감온도 27°</Text>
+          <Text style={{ fontSize: 70, color: "#fff" }}>27°</Text>
+          <Text style={{ fontSize: 20, color: "#fff" }}>체감온도 28°</Text>
           <Text style={{ fontSize: 20, color: "#fff", marginTop: 5 }}>
             최고 <Text style={{ color: "red" }}>29°</Text> 최저{" "}
-            <Text style={{ color: "blue" }}>25°</Text>
+            <Text style={{ color: "blue" }}>26°</Text>
           </Text>
         </View>
         <View
           style={{
             marginTop: 50,
-            alignItems: "center",
-            justifyContent: "center",
             backgroundColor: "#fff",
             borderRadius: 10,
             width: 350,
             height: 200,
           }}
         >
-          <Text>날씨에 맞는 옷 추천</Text>
+          <Text
+            style={{
+              marginVertical: 15,
+              marginLeft: 15,
+              fontWeight: "bold",
+              fontSize: 15,
+            }}
+          >
+            날씨에 맞는 옷 추천
+          </Text>
+          <View
+            style={{ backgroundColor: "black", height: 1, width: "100%" }}
+          ></View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-around",
+              marginTop: 20,
+            }}
+          >
+            <Text>상의</Text>
+            <Text>하의</Text>
+            <Text>기타</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-around",
+              marginTop: 20,
+            }}
+          >
+            <Image source={require("../assets/sleeveless.png")} />
+            <Image source={require("../assets/shorts.png")} />
+            <Image source={require("../assets/sunglasses.png")} />
+          </View>
         </View>
         <View
           style={{
