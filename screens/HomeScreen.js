74e191d22a188b94c5e7a8bb50dc55695951a448
Hwang-yoonji <kaci00@naver.com>
2021-09-27 15:35:19 +0900

    시간별 날씨

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -25,19 +25,18 @@ const HomeScreen = () => {
         </View>
         <View
           style={{
-            marginTop: 50,
+            marginTop: 40,
             backgroundColor: "#fff",
             borderRadius: 10,
             width: 350,
-            height: 200,
+            height: 150,
           }}
         >
           <Text
             style={{
-              marginVertical: 15,
-              marginLeft: 15,
+              marginVertical: 10,
+              marginLeft: 10,
               fontWeight: "bold",
-              fontSize: 15,
             }}
           >
             날씨에 맞는 옷 추천
@@ -49,7 +48,7 @@ const HomeScreen = () => {
             style={{
               flexDirection: "row",
               justifyContent: "space-around",
-              marginTop: 20,
+              marginTop: 10,
             }}
           >
             <Text>상의</Text>
@@ -60,7 +59,7 @@ const HomeScreen = () => {
             style={{
               flexDirection: "row",
               justifyContent: "space-around",
-              marginTop: 20,
+              marginTop: 10,
             }}
           >
             <Image source={require("../assets/sleeveless.png")} />
@@ -71,15 +70,70 @@ const HomeScreen = () => {
         <View
           style={{
             marginTop: 20,
-            alignItems: "center",
-            justifyContent: "center",
             backgroundColor: "#fff",
             borderRadius: 10,
             width: 350,
-            height: 200,
+            height: 170,
           }}
         >
-          <Text>시간별 날씨</Text>
+          <Text
+            style={{
+              marginVertical: 10,
+              marginLeft: 10,
+              fontWeight: "bold",
+            }}
+          >
+            시간별 날씨
+          </Text>
+          <View
+            style={{ backgroundColor: "black", height: 1, width: "100%" }}
+          ></View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-around",
+              marginTop: 15,
+            }}
+          >
+            <Text>지금</Text>
+            <Text>오전 11시</Text>
+            <Text>오후 12시</Text>
+            <Text>오후 1시</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              marginTop: 15,
+            }}
+          >
+            <Image
+              source={require("../assets/sunny.png")}
+              style={{ marginHorizontal: 20 }}
+            />
+            <Image
+              source={require("../assets/sunny.png")}
+              style={{ marginHorizontal: 30 }}
+            />
+            <Image
+              source={require("../assets/sunny.png")}
+              style={{ marginHorizontal: 35 }}
+            />
+            <Image
+              source={require("../assets/sunny.png")}
+              style={{ marginHorizontal: 30 }}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              marginTop: 15,
+            }}
+          >
+            <Text style={{ fontSize: 25, marginHorizontal: 20 }}>27°</Text>
+            <Text style={{ fontSize: 25, marginHorizontal: 20 }}>27°</Text>
+            <Text style={{ fontSize: 25, marginHorizontal: 35 }}>28°</Text>
+            <Text style={{ fontSize: 25, marginHorizontal: 25 }}>29°</Text>
+          </View>
         </View>
         <View
           style={{
