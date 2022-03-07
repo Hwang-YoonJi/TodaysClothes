7385ba311cb96c473d4e654ab8ef071052b19b65
Hwang-yoonji <kaci00@naver.com>
2022-03-07 16:49:03 +0900

    permissions

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Alert } from "react-native";
 import Swiper from "react-native-swiper";
 import HomeScreen from "./screens/HomeScreen";
 import StandardScreen from "./screens/StandardScreen";
@@ -7,9 +7,23 @@ import Loading from "./screens/Loading";
 import * as Location from "expo-location";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    console.log("aaaaaaaaaaaaaa");
+    try {
+      // throw Error();
+      await Location.requestForegroundPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      // console.log(coords.latitude, coords.longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
   };
 
   componentDidMount() {
@@ -17,7 +31,8 @@ export default class extends React.Component {
   }
 
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
 
